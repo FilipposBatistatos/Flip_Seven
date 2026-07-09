2c6baeec55d0749be7c625a12e5e7fea28dd9ed9
Filippos Batistatos <filipposbatistatos@gmail.com>
2026-07-10 09:41:21 +1000

    Added a hand class

---
+++ src/hand.rs
@@ -0,0 +1,167 @@
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
+pub struct Hand(u16);
+
+impl Hand
+{
+    pub fn empty() -> Self
+    {
+        Hand(0)
+    }
+
+    /* Used in exploration so we don't mutate the actual hand */
+    pub fn with(self, value: usize) -> Self
+    {
+        Hand(self.0 | (1 << value))
+    }
+
+    pub fn contains(self, value: usize) -> bool
+    {
+        self.0 & (1 << value) != 0
+    }
+
+    pub fn len(self) -> u32
+    {
+        self.0.count_ones()
+    }
+
+    pub fn score(self) -> i32
+    {
+        let mut total = 0;
+        for v in 0..13u32
+        {
+            if self.0 & (1 << v) != 0
+            {
+                total += v as i32;
+            }
+        }
+        if self.len() >= 7
+        {
+            total += 15;
+        }
+        total
+    }
+}
+
+#[cfg(test)]
+impl Hand
+{
+    fn snapshot(self) -> String
+    {
+        format!(
+            "[{}]\nlen={}\nScore={}",
+            self.get_cards_in_hand(), self.len(), self.score(),
+        )
+    }
+
+    fn get_cards_in_hand(self) -> String
+    {
+        let mut output = String::new();
+
+        for i in 0..13
+        {
+            if (self.0 & (1 << i)) != 0
+            {
+                output.push_str(&format!("{} ", i.to_string()))
+            }
+        }
+        output
+    }
+}
+
+#[cfg(test)]
+mod tests
+{
+    use super::*;
+    use expect_test::expect;
+
+    #[test]
+    fn new_hand()
+    {
+        let hand = Hand::empty();
+
+        let output = hand.snapshot();
+        
+        expect![[r#"
+            []
+            len=0
+            Score=0"#]]
+            .assert_eq(&output);
+    }
+
+    #[test]
+    fn add_cards()
+    {
+        let mut hand = Hand::empty();
+
+        hand = hand.with(2);
+        hand = hand.with(3);
+        hand = hand.with(5);
+
+        let output = hand.snapshot();
+
+        expect![[r#"
+            [2 3 5 ]
+            len=3
+            Score=10"#]]
+            .assert_eq(&output);
+    }
+
+    #[test]
+    fn seven_card_bonus()
+    {
+        let mut hand = Hand::empty();
+
+        for i in 0..7
+        {
+            hand = hand.with(i);
+        }
+
+        let output = hand.snapshot();
+
+        expect![[r#"
+            [0 1 2 3 4 5 6 ]
+            len=7
+            Score=36"#]]
+            .assert_eq(&output);
+    }
+
+    #[test]
+    fn with_makes_copy()
+    {
+        let mut hand = Hand::empty();
+        hand = hand.with(4);
+
+        let newHand = hand.with(5);
+
+        let output = hand.snapshot() + &newHand.snapshot();
+
+        expect![[r#"
+            [4 ]
+            len=1
+            Score=4[4 5 ]
+            len=2
+            Score=9"#]]
+            .assert_eq(&output);
+    }
+
+    // TODO: Better contains testing, perhaps exhaustive
+    #[test]
+    fn contains()
+    {
+        let mut hand = Hand::empty();
+
+        hand.with(4);
+        hand.with(5);
+        hand.with(6);
+
+        let mut output = format!("Contains 7={}", hand.contains(7).to_string());
+
+        hand.with(7);
+        output = format!("{}\nContains 7={}", &output, hand.contains(7).to_string());
+
+        expect![[r#"
+            Contains 7=false
+            Contains 7=false"#]]
+            .assert_eq(&output);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,29 @@
 use std::collections::HashMap;
 
 mod deck;
-mod player;
+mod hand;
 
-fn eval_score(mask: u16) -> i32
-{
-    let mut score = 0;
-    let mut card_count = 0;
-
-    for i in 0..13
-    {
-        if (mask & (1 << i)) != 0
-        {
-            score += i;
-            card_count += 1;
-        }
-    }
-
-    if card_count >= 7
-    {
-        score += 15;
-    }
-    
-    score
-}
-
-fn value(mask: u16, deck: &deck::Deck, cache: &mut HashMap<u16, f64>) -> f64
-{
-    if let Some(v) = cache.get(&mask)
-    {
-        return *v;
-    }
-
-    if mask.count_ones() >= 7
-    {
-        let score = eval_score(mask) as f64;
-        cache.insert(mask, score);
-        return score;
-    }
-
-    let stay_score = eval_score(mask) as f64;
-
-    let mut hit_score = 0.0;
-    let total = deck.get_total_remaining();
-
-    for card in 0..13
-    {
-        if mask & (1 << card) == 0
-        {
-            let p = deck.get_remaining_card(card) as f64 / total as f64;
-            hit_score += p * value(mask | (1 << card), deck, cache);
-        }
-    }
-
-    let ans = stay_score.max(hit_score);
-    cache.insert(mask, ans);
-    ans
-}
+use deck::Deck;
+use hand::Hand;
 
 fn main() {
 
     // setting up the "deck"
-    let mut deck = deck::Deck::new(15);
+    let mut deck = Deck::new(15);
 
     // Setting up player
-    let mut player = player::Player::new();
+    let mut hand = Hand::empty();
 
     // Deal a random card
     loop
     {
-        let mut cache = HashMap::new();
-        let hit_score = value(player.hand, &deck, &mut cache);
         let dealt_card = deck.draw(); 
-        player.give_card(dealt_card);
-        if !player.can_draw
+        if hand.contains(dealt_card)
         {
             println!("Dealt a duplicate: {dealt_card}");
             break;
         }
-        println!("Dealt card: {dealt_card}, predicted score: {hit_score}, score: {}", player.hand_score());
-        
+        hand = hand.with(dealt_card);
+        println!("Dealt card: {dealt_card}, score: {}", hand.score());
     }
 }
