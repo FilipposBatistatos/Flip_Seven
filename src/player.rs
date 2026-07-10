d5ccda923b2508c5e57cc6a4c8a672df7e5192f3
Filippos Batistatos <filipposbatistatos@gmail.com>
2026-07-10 11:56:29 +1000

    Created player class

--- src/player.rs
+++ src/player.rs
@@ -1,178 +1,45 @@
+use crate::hand::Hand;
+use crate::strategy::Strategy;
+
+#[derive(Clone, Copy, PartialEq, Debug)]
+pub enum ControlMode
+{
+    Automatic,
+    Advisory,
+}
+
+#[derive(Clone, Copy, PartialEq, Debug)]
+pub enum PlayerStatus
+{
+    Active,
+    Stayed,
+    Busted,
+}
+
 #[derive(Debug)]
 pub struct Player
 {
-    name: String,
+    pub name: String,
     pub hand: u16,       // Hand mask 
-    cumulative_score: i32,
-    pub can_draw: bool,
+    pub cumulative_score: u32,
+    pub status: PlayerStatus,
+    pub strategy: Box<dyn Strategy>,
+    pub control: ControlMode,
 }
 
 impl Player
 {
-    pub fn new() ->Self
+    pub fn new(name: &str, strategy: Box<dyn Strategy>, control: ControlMode) ->Self
     {
-        Self
+        Player
         {
-            name: "Player".to_string(),
-            hand: 0,
+            name: name.to_string(),
+            hand: Hand::empty(),
             cumulative_score: 0,
-            can_draw: true,
+            status: PlayerStatus::Active,
+            strategy,
+            control,
         }
-    }
-
-    pub fn hand_size(&self) ->u32
-    {
-        self.hand.count_ones()
-    }
-
-    pub fn hand_score(&self) -> i32
-    {
-        let mut score = 0;
-        for card in 0..=12
-        {
-            if (self.hand & (1 << card)) != 0
-            {
-                score += card;
-            }
-        }
-
-        if self.hand_size() >= 7
-        {
-            score += 15;
-        }
-
-        score
-    } 
-
-    pub fn give_card(&mut self, card: usize)
-    {
-        self.hand ^= 1 << card; 
-        if (self.hand & (1 << card)) == 0
-        {
-            self.can_draw = false;
-        }
-    }
-
-    pub fn on_round_end(&mut self)
-    {
-        self.cumulative_score += self.hand_score();
-        self.hand = 0;
-        self.can_draw = true;
     }
 }
 
-#[cfg(test)]
-impl Player
-{
-    fn snapshot(&self) -> String
-    {
-        format!(
-            "Name={}\nScore={}\nHand={} len={}\nHand score={}\nCan draw={}",
-            self.name, self.cumulative_score, self.get_cards_in_hand(), self.hand_size(), self.hand_score(), self.can_draw, 
-        )
-    }
-
-    fn get_cards_in_hand(&self) -> String
-    {
-        let mut output = String::new();
-
-        for i in 0..13
-        {
-            if (self.hand & (1 << i)) != 0
-            {
-                output.push_str(&format!(
-                        "{} ",
-                        i.to_string()
-                    ))
-            }
-        }
-
-        output
-    }
-}
-
-#[cfg(test)]
-mod tests
-{
-    use super::*;
-    use expect_test::expect;
-
-    #[test]
-    fn new_player()
-    {
-        let player = Player::new();
-
-        let output = player.snapshot();
-
-        expect![[r#"
-            Name=Player
-            Score=0
-            Hand= len=0
-            Hand score=0
-            Can draw=true"#]]
-            .assert_eq(&output);
-    }
-
-    #[test]
-    fn hand()
-    {
-        let mut player = Player::new();
-
-        player.give_card(2 as usize);
-        player.give_card(5 as usize);
-        player.give_card(7 as usize);
-
-        let output = player.snapshot();
-        
-        expect![[r#"
-            Name=Player
-            Score=0
-            Hand=2 5 7  len=3
-            Hand score=14
-            Can draw=true"#]]
-            .assert_eq(&output);
-    }
-
-    #[test]
-    fn detect_duplicate()
-    {
-        let mut player = Player::new();
-
-        player.give_card(2 as usize);
-        player.give_card(5 as usize);
-        player.give_card(7 as usize);
-        player.give_card(5 as usize);
-
-        let output = player.snapshot();
-
-        expect![[r#"
-            Name=Player
-            Score=0
-            Hand=2 7  len=2
-            Hand score=9
-            Can draw=false"#]]
-            .assert_eq(&output);
-    }
-
-    #[test]
-    fn round_end()
-    {
-        let mut player = Player::new();
-
-        player.give_card(2 as usize);
-        player.give_card(5 as usize);
-        player.give_card(7 as usize);
-
-        player.on_round_end();
-
-        let output = player.snapshot();
-
-        expect![[r#"
-            Name=Player
-            Score=14
-            Hand= len=0
-            Hand score=0
-            Can draw=true"#]]
-            .assert_eq(&output);
-    }
-}
